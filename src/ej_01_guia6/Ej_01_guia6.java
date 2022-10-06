25d9a04b9e8470dd633c44c077f5bfb108ac7da7
Celina Segundo <celinasegundo21@gmail.com>
2022-10-06 19:42:09 -0300

    tercer commit

--- src/ej_01_guia6/Ej_01_guia6.java
+++ src/ej_01_guia6/Ej_01_guia6.java
@@ -10,27 +10,22 @@ package ej_01_guia6;
 import entidades.Libro;
 import serviciosDeLibros.metodoConstructor;
 
-/**
- *
- * @author Celi
- */
+
 public class Ej_01_guia6 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         
         System.out.println("Bienvenido al ejercicio 01 de la guia 7, Programacion Orientada a Objetos (POO).");
         System.out.println("Vamos a ingresar los datos de un libro para alojarlo como objeto.");
         System.out.println("Necesito que completes la informacion para llenar sus atributos.");
         
-        //Scanner leer = new Scanner(System.in);
+       
         
         metodoConstructor aa = new metodoConstructor();
         Libro L1 = aa.crearLibro();
         aa.mostrarInfo(L1);
-        //System.out.println(L1.toString());
+        
         
     }
     
--- src/serviciosDeLibros/metodoConstructor.java
+++ src/serviciosDeLibros/metodoConstructor.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package serviciosDeLibros;
 
 import entidades.Libro;
 import java.util.Scanner;
 
-/**
- *
- * @author Alumno
- */
+
 public class metodoConstructor {
    Scanner leer = new Scanner(System.in).useDelimiter("\n");
     
